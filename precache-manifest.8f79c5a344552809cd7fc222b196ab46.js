14431a4f625395053956971097f54e751f6eb2b7
Gurudeep <gurudeep88@gmail.com>
2020-05-24 23:26:52 +0530

    Updates

---
+++ precache-manifest.8f79c5a344552809cd7fc222b196ab46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bd60e4ab97ea9c60a03fd608ead8cd1",
+    "url": "/movie-search-app/index.html"
+  },
+  {
+    "revision": "ced250eb3f20bc7d92c2",
+    "url": "/movie-search-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f5de57ac9b16dc02d59a",
+    "url": "/movie-search-app/static/js/2.9d2bda8e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie-search-app/static/js/2.9d2bda8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ced250eb3f20bc7d92c2",
+    "url": "/movie-search-app/static/js/main.fbc227ad.chunk.js"
+  },
+  {
+    "revision": "be03855a5ff4b148ffe9",
+    "url": "/movie-search-app/static/js/runtime-main.6ccfa030.js"
+  },
+  {
+    "revision": "6d2c7103c3fc4a085ce84135ec1b905a",
+    "url": "/movie-search-app/static/media/spinner.6d2c7103.gif"
+  }
+]);
